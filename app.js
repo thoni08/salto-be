94ca90c6172f01418bfb3d39c7564b0af723c411
Fathoni <88326103+thoni08@users.noreply.github.com.>
2026-04-19 11:21:01 +0700

    feat: initialize Express server with health check and error handling

---
+++ app.js
@@ -0,0 +1,28 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import errorHandler from './middlewares/error.middleware.js';
+
+import testRoutes from './routes/test.route.js';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'OK', message: 'Server is running perfectly' });
+});
+
+app.use('/api/test', testRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).json({ success: false, error: 'Route not found' });
+});
+
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
